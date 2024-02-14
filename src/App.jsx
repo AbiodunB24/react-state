db1122c6430268d07fae536bd0d965c77c88b945
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-02-14 20:59:41 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+//Class-based component...
+class App extends Component {
+  state = {
+    person: {
+      fullName: "Balogun Babatunde",
+      
+      //Little expression of who Adegbite Muiz is.
+      bio: "Babatunde is a young, vibrant, intellectual and passionate web developer who's striving to make wave in the digital world, He likes taking on new challenges and meeting people, His journey as a web developer is marked by his passion to take on new challenges, dedication and relentless curiosity.",
+      imgSrc: "./images/AA0F9AB0-9294-4DA2-A058-E2858478BD66.JPG",
+      profession: "Fullstack Developer",
+    },
+    show: false,
+    intervalId: null,
+    timeElapsed: 0,
+  };
+
+  //Interval...
+  componentDidMount() {
+    const intervalId = setInterval(this.updateTimeElapsed, 1000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  //Time Elapsed...
+  updateTimeElapsed = () => {
+    this.setState((prevState) => ({
+      timeElapsed: prevState.timeElapsed + 1,
+    }));
+  };
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeElapsed } = this.state;
+
+    return (
+      <div style={{ background: "linear-gradient(to right, gray, skyblue)", width:"100vw", height:"100vh", flex:"row", justifyContent:"center", alignItems:"center", paddingLeft:"400px", paddingTop:"100px"}}>
+        <button style={{ color: "red", flex:"row", justifyContent:"center", alignItems:"center" }} onClick={this.toggleShow}>
+          Toggle Profile
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <img
+              style={{ height: 250, width: 250, padding: 20 }}
+              src={imgSrc}
+              alt="Avatar"
+            />
+            <p style={{ color: "black", width:"500px"}}> Bio: {bio}</p>
+            <p style={{ color: "purple" }}>Profession: {profession}</p>
+          </div>
+        )}
+        <p>Time Elapsed: {timeElapsed} seconds</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
